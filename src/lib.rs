043ad97e90ed15796ed42e8a1fcc87dc7b50da98
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2019-10-19 16:07:09 -0400

    array addition function

--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,21 @@ mod tests {
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
+
+    #[test]
+    fn test_array_addition() {
+        use crate::array_addition;
+
+        assert_eq!(array_addition(&[1, 2], &[2, 1]), &[3, 3])
+    }
+}
+
+
+pub fn array_addition(a: &[i32], b:&[i32]) -> Vec<i32> {
+    assert_eq!(a.len(), b.len(), "The vectors are not the same length.");
+    let mut c: Vec<i32> = vec![0; b.len()];
+    for item in 0..a.len() {
+        c[item] = a[item] + b[item];
+    }
+    c
 }
